8219252aa825599f96008b79f82a08714960d37b
tinama <750457103@qq.com>
2020-08-05 11:14:05 +0800

    add]

--- app/src/main/java/yzx/app/editer/pages/PureColorPage.kt
+++ app/src/main/java/yzx/app/editer/pages/PureColorPage.kt
@@ -1,7 +1,11 @@
 package yzx.app.editer.pages
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.TextView
+import kotlinx.android.synthetic.main.page_pure_color.*
+import yzx.app.editer.R
 import yzx.app.editer.pages.abs.BaseEditPage
 import yzx.app.editer.util.U
 
@@ -17,7 +21,27 @@ class PureColorPage : BaseEditPage() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.page_pure_color)
+        makeShapeCheckbox()
+    }
 
+
+    private val shapeSelectedTag = "fuckyourmother_"
+    private val shapeViews: Array<TextView> by lazy {
+        arrayOf(shapeRect, shapeCircle, shapeTriangle)
+    }
+
+    private fun makeShapeCheckbox() {
+        shapeViews.forEach { setShapeCheckBoxBG(it, it == shapeRect) }
+        shapeRect.setOnClickListener { shapeViews.forEach { setShapeCheckBoxBG(it, it == shapeRect) } }
+        shapeCircle.setOnClickListener { shapeViews.forEach { setShapeCheckBoxBG(it, it == shapeCircle) } }
+        shapeTriangle.setOnClickListener { shapeViews.forEach { setShapeCheckBoxBG(it, it == shapeTriangle) } }
+    }
+
+    private fun setShapeCheckBoxBG(view: TextView, selected: Boolean) {
+        view.tag = if (selected) shapeSelectedTag else null
+        view.setTextColor(if (selected) Color.parseColor("#89c3fd") else Color.parseColor("#999999"))
+        view.setBackgroundResource(if (selected) R.drawable.bg_checkbox_single_selected else R.drawable.bg_checkbox_single_normal)
     }
 
 }
--- app/src/main/java/yzx/app/editer/pages/abs/BaseEditPage.kt
+++ app/src/main/java/yzx/app/editer/pages/abs/BaseEditPage.kt
@@ -1,9 +1,11 @@
 package yzx.app.editer.pages.abs
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.blankj.utilcode.util.BarUtils
 import com.blankj.utilcode.util.ScreenUtils
+import yzx.app.editer.R
 
 
 open class BaseEditPage : AppCompatActivity() {
@@ -11,6 +13,7 @@ open class BaseEditPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         BarUtils.setStatusBarLightMode(window, true)
+        findViewById<View>(R.id.back)?.setOnClickListener { finish() }
 
     }
 
