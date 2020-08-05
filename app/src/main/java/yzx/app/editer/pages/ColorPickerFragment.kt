cfa3809b88ae93f4f57347275f6acdc3afaa7255
tinama <750457103@qq.com>
2020-08-05 15:30:31 +0800

    add]

--- app/src/main/java/yzx/app/editer/pages/ColorPickerFragment.kt
+++ app/src/main/java/yzx/app/editer/pages/ColorPickerFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_color_picker.*
 import yzx.app.editer.R
 
 
@@ -15,6 +16,15 @@ class ColorPickerFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_color_picker, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.setBackgroundColor(colorBar.currentColor)
+        colorBar.colorCallback = { c ->
+            view.setBackgroundColor(c)
+        }
     }
 
 }
