2c77f5ec9f80cc4add67dffe07ed934fdbafdb5b
tinama <750457103@qq.com>
2020-08-06 17:33:26 +0800

    add]

--- app/src/main/java/yzx/app/editer/pages/ColorPickerFragment.kt
+++ app/src/main/java/yzx/app/editer/pages/ColorPickerFragment.kt
@@ -4,26 +4,26 @@ import android.content.res.ColorStateList
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_color_picker.*
 import yzx.app.editer.R
 import yzx.app.editer.util.tools.Inte
 import yzx.app.editer.util.tools.inverseColor
+import yzx.app.editer.util.tools.replaceColorAlpha
 import java.math.BigInteger
+import java.util.*
 
 
 class ColorPickerFragment : Fragment() {
 
-    lateinit var onComplete: (Int) -> Unit
-    var currentColor: Int = Color.WHITE
-        private set
-
-    fun setInitColor(color: Int) {
-        currentColor = color
-    }
+    var onComplete: ((Int) -> Unit)? = null
+    var initColor: Int = Color.WHITE
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -33,16 +33,45 @@ class ColorPickerFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         activity?.window?.statusBarColor = Color.parseColor("#f5f5f5")
+        acceptColor(initColor)
+        panel.colorCallback = { byUser ->
+            if (byUser) {
+                val c = replaceColorAlpha(panel.currentColor, alphaBar.currentAlpha)
+                setRGBAInput(c)
+                colorCircle.color = c
+                setHexText(c)
+            }
+        }
+        colorBar.colorCallback = { byUser ->
 
-        panel.given(currentColor)
-        setRGBAInput(currentColor)
-        colorBar.given(currentColor)
-        alphaBar.set(Color.alpha(currentColor) / 255f, currentColor)
-        setHexText(currentColor)
-        setConfirmButton(currentColor)
+        }
+        alphaBar.alphaCallback = { byUser ->
 
+        }
+        val textWatcher = object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) = Unit
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) = Unit
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+        }
+        redInput.addTextChangedListener(textWatcher)
+        greenInput.addTextChangedListener(textWatcher)
+        blueInput.addTextChangedListener(textWatcher)
+        confirm.setOnClickListener {
+
+        }
     }
 
+
+    private fun acceptColor(color: Int) {
+        panel.given(color)
+        setRGBAInput(color)
+        colorBar.given(color)
+        alphaBar.set(Color.alpha(color) / 255f, color)
+        setHexText(color)
+        colorCircle.color = color
+    }
 
     private fun setRGBAInput(color: Int) {
         redInput.setText(Color.red(color).toString())
@@ -52,11 +81,8 @@ class ColorPickerFragment : Fragment() {
     }
 
     private fun setHexText(color: Int) {
-        hexText.text = Inte.toHex(color).toUpperCase()
+        hexText.text = Inte.toHex(color).toUpperCase(Locale.ROOT)
     }
 
-    private fun setConfirmButton(color: Int) {
-        confirm.supportBackgroundTintList = ColorStateList.valueOf(color)
-    }
 
 }
--- app/src/main/java/yzx/app/editer/util/tools/Tool.kt
+++ app/src/main/java/yzx/app/editer/util/tools/Tool.kt
@@ -6,3 +6,8 @@ import android.graphics.Color
 fun inverseColor(c: Int): Int {
     return Color.rgb(255 - Color.red(c), 255 - Color.green(c), 255 - Color.blue(c))
 }
+
+
+fun replaceColorAlpha(color: Int, alpha: Float): Int {
+    return Color.argb((alpha * 255f).toInt(), Color.red(color), Color.green(color), Color.blue(color))
+}
