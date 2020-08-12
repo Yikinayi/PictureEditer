24fe8e57a5f33b3d7bb4eae492ca5ba71aef7ef1
tinama <750457103@qq.com>
2020-08-12 15:32:55 +0800

    add]

--- app/src/main/java/yzx/app/editer/MainActivity.kt
+++ app/src/main/java/yzx/app/editer/MainActivity.kt
@@ -1,13 +1,9 @@
 package yzx.app.editer
 
-import android.os.Build
 import android.os.Bundle
-import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.blankj.utilcode.util.BarUtils
 import kotlinx.android.synthetic.main.activity_main.*
-import yzx.app.editer.pages.ColorPickerFragment
 import yzx.app.editer.pages.MainEditFragment
 
 
@@ -21,12 +17,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         if (!isFinishing)
-            supportFragmentManager.beginTransaction().replace(R.id.container, ColorPickerFragment()).commitAllowingStateLoss()
+            supportFragmentManager.beginTransaction().replace(R.id.container, MainEditFragment()).commitAllowingStateLoss()
 
         cacheView.setOnClickListener {
 
         }
     }
-
 
 }
---
+++ app/src/main/java/yzx/app/editer/pages/ability/ColorPicker.kt
@@ -0,0 +1,66 @@
+package yzx.app.editer.pages.ability
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.SystemClock
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import androidx.appcompat.app.AppCompatActivity
+import yzx.app.editer.pages.ColorPickerFragment
+import yzx.app.editer.util.U
+
+object ColorPicker {
+    val callbacks = HashMap<String, (Int) -> Unit>()
+    fun start(initColor: Int? = null, callback: (Int) -> Unit) {
+        val sign = SystemClock.uptimeMillis().toString()
+        callbacks[sign] = callback
+        U.app.startActivity(Intent(U.app, ColorPickerActivity::class.java).apply {
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            initColor?.apply { putExtra("color", this) }
+            putExtra("sign", sign)
+        })
+    }
+}
+
+
+class ColorPickerActivity : AppCompatActivity() {
+
+    private val initColor: Int by lazy { intent.getIntExtra("sign", 0) }
+    private val sign by lazy { intent.getStringExtra("sign") ?: "" }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (sign.isBlank()) {
+            finish()
+        }
+
+        //
+        else {
+
+            val id = View.generateViewId()
+            setContentView(FrameLayout(this).apply {
+                layoutParams = ViewGroup.LayoutParams(-1, -1)
+                this.id = id
+            })
+
+            val f = ColorPickerFragment()
+            if (initColor != 0)
+                f.initColor = initColor
+            f.onComplete = { color ->
+                finish()
+                ColorPicker.callbacks.remove(sign)?.invoke(color)
+            }
+            supportFragmentManager.beginTransaction().replace(id, f).commitAllowingStateLoss()
+        }
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        ColorPicker.callbacks.remove(sign)
+    }
+
+}
