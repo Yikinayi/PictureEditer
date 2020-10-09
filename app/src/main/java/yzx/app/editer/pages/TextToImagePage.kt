fa81fe73f1d4c3819bc01ecb64185a3700c7bc78
yangzhongxu <yangzhongxu@okay.cn>
2020-10-09 17:40:21 +0800

    add]

--- app/src/main/java/yzx/app/editer/pages/TextToImagePage.kt
+++ app/src/main/java/yzx/app/editer/pages/TextToImagePage.kt
@@ -2,6 +2,7 @@ package yzx.app.editer.pages
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
 import android.graphics.Color
 import android.os.Bundle
 import android.view.View
@@ -10,11 +11,15 @@ import com.blankj.utilcode.util.BarUtils
 import kotlinx.android.synthetic.main.page_text_to_image.*
 import yzx.app.editer.R
 import yzx.app.editer.pages.ability.ColorPicker
+import yzx.app.editer.pages.ability.ImageProcessCallback
+import yzx.app.editer.pages.ability.startImageCacheProcess
+import yzx.app.editer.pages.ability.startImageSaveProcess
 import yzx.app.editer.util.U
 import yzx.app.editer.util.dialog.IntRangeSelectAlert
 import yzx.app.editer.util.dialog.SimpleConfirmAlert
 import yzx.app.editer.util.dp2px
 import yzx.app.editer.util.tools.setOnClickListenerPreventFast
+import yzx.app.editer.widget.toast.toast
 
 
 class TextToImagePage : AppCompatActivity() {
@@ -43,8 +48,48 @@ class TextToImagePage : AppCompatActivity() {
         textColorButton.setOnClickListenerPreventFast { ColorPicker.start { input.setTextColor(it) } }
         clearButton.setOnClickListenerPreventFast { if (input.text.isNotEmpty()) SimpleConfirmAlert.show(this, "清空?", "手误", "确定") { input.setText("") } }
         textSizeButton.setOnClickListenerPreventFast { showSelectedSizeMenu { input.textSize = it.toFloat() } }
-
+        saveButton.setOnClickListenerPreventFast {
+            if (input.text.isEmpty()) {
+                toast("先输入内容再操作")
+                return@setOnClickListenerPreventFast
+            }
+            startSave()
+        }
+        cacheButton.setOnClickListenerPreventFast {
+            if (input.text.isEmpty()) {
+                toast("先输入内容再操作")
+                return@setOnClickListenerPreventFast
+            }
+            startCache()
+        }
     }
+
+    private fun startSave() {
+        input.drawingCacheQuality = View.DRAWING_CACHE_QUALITY_LOW
+        input.buildDrawingCache()
+        val bmp = input.drawingCache
+        startImageSaveProcess(this, object : ImageProcessCallback {
+            override fun getBitmap(): Bitmap? = bmp
+            override fun recycleBitmap(): Boolean = false
+            override fun onComplete(result: Boolean) {
+                input?.destroyDrawingCache()
+            }
+        })
+    }
+
+    private fun startCache() {
+        input.drawingCacheQuality = View.DRAWING_CACHE_QUALITY_LOW
+        input.buildDrawingCache()
+        val bmp = input.drawingCache
+        startImageCacheProcess(this, object : ImageProcessCallback {
+            override fun getBitmap(): Bitmap? = bmp
+            override fun recycleBitmap(): Boolean = false
+            override fun onComplete(result: Boolean) {
+                input?.destroyDrawingCache()
+            }
+        })
+    }
+
 
     private var nowTextSize = 15
 
